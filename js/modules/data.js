996d583255842c871ea4a421b87dd244e1839717
Luiza <luizabenevides2007@gmail.com>
2026-03-04 19:48:42 -0300

    Data refatorada

--- js/modules/data.js
+++ js/modules/data.js
@@ -1,44 +1,47 @@
-export default function dataTarefa() {
-  let cesta = JSON.parse(localStorage.getItem("cesta")) || [];
+export default class dataTarefa {
+  constructor(containerTarefa) {
+    this.tarefaContainer =  document.querySelector(containerTarefa);
+    this.cesta = JSON.parse(localStorage.getItem("cesta")) || [];
+    this.prazoLista = [];
+    this.prazoListaSpan = [];
+  };
 
-  let prazoLista = [];
-  function criarListaPrazo() {
-    cesta.forEach((elemento) => {
-      prazoLista.push(elemento.prazo);
+  criarListaPrazo() {
+    this.cesta.forEach((elemento) => {
+      this.prazoLista.push(elemento.prazo);
     });
-  }
-  criarListaPrazo();
+    this.prazoListaSpan = [...this.prazoLista];
+  };
 
-  function validarPrazo(item) {
+  validarPrazo(item) {
     item = item.replace(/(\d{2})\/(\d{2})\/(\d{4})/g, `$3/$2/$1`);
     return item;
-  }
+  };
 
-  function prazoVerificar(dataPrazo) {
+  prazoVerificar(dataPrazo) {
     const hoje = new Date();
     const alvo = new Date(dataPrazo);
     const diferenca = alvo - hoje;
 
     const dias = Math.ceil(diferenca / (1000 * 60 * 60 * 24));
     return dias;
-  }
+  };
 
-  let prazoListaSpan = [...prazoLista];
-  function prazoHtml() {
+  prazoHtml() {
     const prazoTarefa = document.querySelectorAll(".p-item");
 
-    prazoListaSpan.forEach((prazo, index) => {
-      prazoListaSpan[index] = prazo.replace(
+    this.prazoListaSpan.forEach((prazo, index) => {
+      this.prazoListaSpan[index] = prazo.replace(
         /(\d{4})-(\d{2})-(\d{2})/g,
         "$3/$2/$1",
       );
     });
     prazoTarefa.forEach((itemTarefa) => {
       const dataDaTarefa = itemTarefa.firstChild.textContent.trim();
-      prazoListaSpan.forEach((prazo) => {
+      this.prazoListaSpan.forEach((prazo) => {
         if (prazo === dataDaTarefa) {
           const span = itemTarefa.querySelector("span");
-          const resultado = prazoVerificar(validarPrazo(prazo));
+          const resultado = this.prazoVerificar(this.validarPrazo(prazo));
           if (resultado < -0) {
             span.classList.add("atrasou");
             span.classList.remove("hoje");
@@ -55,10 +58,17 @@ export default function dataTarefa() {
             span.classList.remove("hoje");
             span.classList.remove("atrasou");
             span.innerHTML = `Faltam ${resultado} dias`;
-          }
+          };
         }
       });
     });
-  }
-  prazoHtml();
+  };
+
+  init() {
+    if (this.tarefaContainer) {
+      this.criarListaPrazo();
+      this.prazoHtml();
+    };
+    return this;
+  };
 }
